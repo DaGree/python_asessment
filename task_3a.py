89e13b435acc757c79def65bf796969df1e9f7b5
DaGree <mr.dan6886@yandex.ru>
2021-09-28 19:07:31 +0300

    create simple get request

--- task_3a.py
+++ task_3a.py
@@ -6,6 +6,14 @@ def get_token():
     config.read("settings.ini")  # читаем конфиг
     return config["Weather"]["token"]
 
-print(get_token())
+#print(get_token())
+token=get_token()
+#print(token)
 city=input("Enter your own city \n>")
-print("Your own city is "+city)
+#print("Your own city is "+city)
+endpoint = "http://api.openweathermap.org/data/2.5/weather"
+query_params = {"appid":token, "q":city, "units":"metric", "lang":"ru"}
+
+response = requests.get(endpoint, params=query_params)
+
+print(response.text)
