b676f905cdc54bc7282ea58d09c089feda2e4895
DaGree <mr.dan6886@yandex.ru>
2021-10-01 18:45:50 +0300

    add second fuction with sunrise

--- task_3a.py
+++ task_3a.py
@@ -1,6 +1,8 @@
 import configparser
 import requests
 import json
+import datetime
+import time
 
 def get_token():
     config = configparser.ConfigParser()  # создаём объекта парсера
@@ -13,28 +15,64 @@ def input_city():
     return(city)
 
 def delta_temp_day(description):
-    delta=[5]
+    deltak=[5]
     omega=[5]
+    lenght=len(description["list"])
     for i in range(lenght):
         data=description["list"][i]["dt_txt"]
         if data.endswith("00:00:00",11):
             omega.append(i)
             temp_min=float(description["list"][i]["main"]["temp"])
             feels_like=float(description["list"][i]["main"]["feels_like"])
-            delta.append(abs(temp_min-feels_like))
-    id_omega=delta.index(min(delta))
+            deltak.append(abs(temp_min-feels_like))
+    id_omega=deltak.index(min(deltak))
     id_list=omega[id_omega]
-    print(min(delta)," °C - минимальная разница температур ночью ")
+    print(min(deltak)," °C - минимальная разница температур ночью ")
     print("Дата дня с минимальной разницей",description["list"][id_list]["dt_txt"][:10])
+
+def request_to_api(token,city):
+    endpoint = "http://api.openweathermap.org/data/2.5/forecast"          #forecast
+    query_params = {"appid":token, "q":city, "units":"metric", "lang":"ru", "mode":"json"}
+    response = requests.get(endpoint, params=query_params)
+    description=json.loads(response.text)
+   
+    return(description)
+
+def max_sun(description):
+    deltak=[5]
+    omega=[5]
+    lat=float(description["city"]["coord"]["lat"])
+    lon=float(description["city"]["coord"]["lon"])
+    endpoint = "http://api.openweathermap.org/data/2.5/onecall"         
+    query_params = {"appid":token,"lat":lat, "lon":lon,"exclude":"hourly,current","units":"metric", "lang":"ru", "mode":"json"}
+    response=requests.get(endpoint,params=query_params)
+    answer=json.loads(response.text)
+
+    for i in range (5):
+        sunrise= datetime.datetime.fromtimestamp(answer["daily"][i]["sunrise"])
+        sunset= datetime.datetime.fromtimestamp(answer["daily"][i]["sunset"])
+        delta=(sunset-sunrise)
+        deltak.append(delta.seconds)
+
+    max_index=deltak.index(max(deltak))
+    ty_res = time.gmtime(deltak[max_index])
+    res = time.strftime("%H:%M:%S",ty_res)
+
+    voshod= datetime.datetime.fromtimestamp(answer["daily"][max_index]["sunrise"])
+    print("Восход")
+    print(voshod.strftime('%H:%M:%S'))
+
+    zakat= datetime.datetime.fromtimestamp(answer["daily"][max_index]["sunset"])
+    print("Закат")
+    print(zakat.strftime('%H:%M:%S'))
+
+    print("Максимальная продолжительность дня соcтавила")
+    print(res)
+    print("По состоянию на ")
+    print(zakat.strftime('%Y-%m-%d'))
 
 token=get_token()
 city=input_city()
-
-endpoint = "http://api.openweathermap.org/data/2.5/forecast"
-query_params = {"appid":token, "q":"Moscow", "units":"metric", "lang":"ru", "mode":"json"}
-
-response = requests.get(endpoint, params=query_params)
-description=json.loads(response.text)
-lenght=len(description["list"])
-
+description=request_to_api(token,city)
 delta_temp_day(description)
+max_sun(description)
