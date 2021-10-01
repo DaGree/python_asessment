52f7510d8a7572ae6a40d19acce12634f138ced2
DaGree <mr.dan6886@yandex.ru>
2021-10-01 15:49:59 +0300

    day with minimal delta temp

--- task_3a.py
+++ task_3a.py
@@ -14,8 +14,27 @@ token=get_token()
 city="Moscow"
 #print("Your own city is "+city)
 endpoint = "http://api.openweathermap.org/data/2.5/forecast"
-query_params = {"appid":token, "q":"Moscow", "units":"metric", "lang":"ru"}
+query_params = {"appid":token, "q":"Moscow", "units":"metric", "lang":"ru", "mode":"json"}
 
-response = requests.get(endpoint, params=query_params).json()
-description=response
-print(description)
+response = requests.get(endpoint, params=query_params)
+description=json.loads(response.text)
+lenght=len(description["list"])
+delta=[5]
+omega=[5]
+for i in range(lenght):
+    data=description["list"][i]["dt_txt"]
+    if data.endswith("00:00:00",11):
+        omega.append(i)
+        temp_min=float(description["list"][i]["main"]["temp"])
+        feels_like=float(description["list"][i]["main"]["feels_like"])
+        delta.append(abs(temp_min-feels_like))
+        print(data)
+        print(description["list"][i]["main"]["temp"])
+        print(description["list"][i]["main"]["feels_like"])
+
+print(delta)
+id_omega=delta.index(min(delta))
+id_list=omega[id_omega]
+print(omega)
+print(min(delta)," °C - минимальная разница температур ночью ")
+print("Дата дня с минимальной разницей",description["list"][id_list]["dt_txt"][:10])
